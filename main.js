9fa7a66d097aec1355f6b9ae250d086e0f1c130d
longnm <long.nm@anlab.biz>
2020-12-31 09:59:57 +0700

    Implement module manage lunar event: init database

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const { app } = require('electron')
+const { databaseHelper } = require('./resources/js/database/database-helper.js');
+const fs = require('fs');
+const { app } = require('electron');
+const path = require('path');
 
-let windowProvider = null
-let menu = null
+let windowProvider = null;
+let menu = null;
+let dbname = "db.sqlite";
 
-const gotLock = app.requestSingleInstanceLock()
+let dbHelper = new databaseHelper(path.join(__dirname, dbname));
+
+const gotLock = app.requestSingleInstanceLock();
 
 if (!gotLock) {
   app.exit(0)
@@ -27,13 +33,18 @@ app.on('before-quit', () => {
   app.exit(0)
 })
 
-// app.userAgentFallback = app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'
+app.userAgentFallback = app.userAgentFallback = 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136'
 
 function createWindow() {
+  // dbHelper.insertEvent("2020/12/31",false)
+  // dbHelper.selectEvent(1).then((events) => {
+  //   console.log(events)
+  // })
   initialize()
   if (windowProvider.getWindow() == null) {
     windowProvider.createMainWindow()
     menu.buildMenu(windowProvider)
+
   } else {
     if (process.platform === 'darwin') {
       app.dock.show()
--- resources/js/database/database-helper.js
+++ resources/js/database/database-helper.js
@@ -1,10 +1,43 @@
 (function () {
-  const path = require('path');
-  const knex = require('knex')({
-    client: "sqlite3",
-    connection: {
-      filename: path.join(__dirname, 'database.sqlite')
+  function databaseHelper(pathFile) {
+    const knex = require('knex')({
+      client: 'sqlite3',
+      connection: {
+        filename: pathFile,
+      },
+      useNullAsDefault: true,
+    });
+
+    this.insertEvent = function(datetimeEvent, isLeapYear) {
+      knex('events').insert({lunar_date: datetimeEvent, leap_year: isLeapYear})
+      .catch(e => {
+        console.error(e)
+      })
     }
-  });
+
+    this.selectEvent = function(id) {
+      return knex('events').select('*')
+    }
+
+    knex.schema.hasTable('events').then(function(exists) {
+      /*init tables here*/
+      if (!exists) {
+        knex.schema.createTable('events', table => {
+          table.increments('id').primary();
+          table.datetime('lunar_date');
+          table.boolean('leap_year');
+        })
+        .catch(e => {
+          console.error(e);
+        });
+      }
+    })
+    .catch(e => {
+      console.error(e);
+    });
+
+  }
+
+  module.exports.databaseHelper = databaseHelper
 }())
 
